362095577b2ed4d4bf61d5b4526fdfaed4e262c6
minghao.ong.2014 <minghao.ong.2014@sis.smu.edu.sg>
2017-04-30 04:00:48 +0800

    did alot of changesss

--- js/index.js
+++ js/index.js
@@ -3,20 +3,41 @@ $(document).ready(function () {
     setTimeout(setupTimeOut, 1000);
 });
 
-$(".login-submit").click(function () {
-    $(".login-submit").html("<img src=\"img/loading.gif\" style=\"height:100%; margin: 0px auto;\" class=\"img-responsive\" />");
-
-    setTimeout(loginSlideUp, 2000);
-});
-
 function loginSlideUp() {
     $(".cont").slideUp(1000);
 }
 
 function setupTimeOut() {
-    console.log("setup");
     $("#card-1").click(function () {
-        console.log("clicked modal");
         $('#myModal').modal('toggle');
     });
+    
+    $("#propose-chat-button").click(function () {
+        $('#proposeModal').modal('toggle');
+    });
+
+    $(".login-submit").click(function () {
+        $(".login-submit").html("<img src=\"img/loading.gif\" style=\"height:100%; margin: 0px auto;\" class=\"img-responsive\" />");
+
+        setTimeout(loginSlideUp, 2000);
+    });
+    
+    $(".nav-newproject").click(function () {
+        console.log("go to new project");
+    });
+
+    $(".nav-project").click(function () {
+        console.log("go to existing project");
+    });
+
+    $(".nav-findmentors").click(function () {
+        window.location.href = 'findmentor.html';
+    });
+
 }
+
+$("#send-message-button").click(function() {
+    console.log("send message");
+});
+
+    
