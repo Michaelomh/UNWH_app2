fc44bb1269f39d9ba045709b3790832d84002c99
minghao.ong.2014 <minghao.ong.2014@sis.smu.edu.sg>
2017-04-30 01:18:08 +0800

    added find mentor

--- js/index.js
+++ js/index.js
@@ -1,8 +1,22 @@
 $(document).ready(function () {
-
+    console.log("ready up");
+    setTimeout(setupTimeOut, 1000);
 });
-
 
 $(".login-submit").click(function () {
     $(".login-submit").html("<img src=\"img/loading.gif\" style=\"height:100%; margin: 0px auto;\" class=\"img-responsive\" />");
+
+    setTimeout(loginSlideUp, 2000);
 });
+
+function loginSlideUp() {
+    $(".cont").slideUp(1000);
+}
+
+function setupTimeOut() {
+    console.log("setup");
+    $("#card-1").click(function () {
+        console.log("clicked modal");
+        $('#myModal').modal('toggle');
+    });
+}
